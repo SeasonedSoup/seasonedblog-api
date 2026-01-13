d656b7f9634124d57239e64686237a60880d4619
SeasonedSoup <emmetteagoncillo1@gmail.com>
2026-01-14 06:59:43 +0800

    Add hash on user controllers and createPost func
    also added a comment controller.js for the comments

--- controllers/blogPostController.js
+++ controllers/blogPostController.js
@@ -1,5 +1,16 @@
 const { prisma } = require("../lib/prisma")
 
 async function postBlogPost(req, res) {
-    
+    await prisma.post.create({
+        data: {
+            id: req.body.id,
+            title: req.body.title,
+            content: req.body.content
+           // authorId: req.params.userId TBI*
+        }
+    })
 }
+
+module.exports = {
+    postBlogPost
+};
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,11 @@
 const { prisma } = require("../lib/prisma.js")
-
+const bcryptjs = require("bcryptjs")
 async function createUser(req, res) {
+    const hashedPass = await bcryptjs.hash(req.body.password ,11) 
     await prisma.user.create({
         data: {
-            password: req.body.password,
+            id: req.body.id,
+            password: hashedPass,
             email: req.body.email,
             name: req.body.name
         }

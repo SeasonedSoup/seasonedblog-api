6589eea8627968f1f6626baa34dd1cd5d2f00782
SeasonedSoup <emmetteagoncillo1@gmail.com>
2026-01-15 06:06:28 +0800

    Add login authenthication for jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
-const { prisma } = require("../lib/prisma.js")
-const bcryptjs = require("bcryptjs")
+const { prisma } = require("../lib/prisma.js");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");const { use } = require("react");
+;
 
 async function createUser(req, res) {
     const hashedPass = await bcryptjs.hash(req.body.password ,11) 
@@ -10,7 +12,32 @@ async function createUser(req, res) {
             email: req.body.email,
             name: req.body.name
         }
+    });
+};
+
+async function loginUser(req, res) {
+    const user = await prisma.user.findUnique({
+        where: {
+            email: req.body.email
+        }
     })
+
+    if (!user) {
+        return res.status(404).json({error: "User does not exist"});
+    }
+
+    const matchResult = await bcryptjs.compare(req.body.password, user.password);
+
+    if (!matchResult) {
+        return res.status(401).json({error: "Wrong username or password"});
+    }
+    jwt.sign({userId: user.id}, 'secretkey', (err, token) => {
+        if (err) {
+            return res.status(500).json({ error: 'Token error' });
+        }
+        
+        res.json(token);
+    });
 }
 
 module.exports = {
